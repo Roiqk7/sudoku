564ae1851f75f6f8039f7d0c7408e764c347f89d
Roiqk7 <petrsevecek@icloud.com>
2024-11-09 18:40:11 +0100

    test

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -8,6 +8,9 @@ Description: This file contains the main function.
 
 // Delete - Test
 #include "../include/grid.hpp"
+#include "../include/solver.hpp"
+#include <array>
+
 
 int main()
 {
@@ -19,6 +22,16 @@ int main()
 
         // Delete - Test
         Sudoku::Grid grid;
+        grid.print();
+
+        std::array<int, 9> rowArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        grid.setBox(0, rowArray);
+        grid.setBox(4, rowArray);
+        grid.setBox(8, rowArray);
+        grid.print();
+
+        Sudoku::Solver solver;
+        solver.solve(grid);
         grid.print();
 
         return 0;
