411ff888b9167d7537dae35a088425ec5ca31b36
Roiqk7 <petrsevecek@icloud.com>
2024-11-10 14:21:48 +0100

    Added basic framework

---
+++ src/gui.cpp
@@ -0,0 +1,50 @@
+/*
+Date: 10/11/2024
+
+GUI class controls the graphical user interface of the program.
+*/
+
+#include "../include/gui.hpp"
+
+namespace System
+{
+// Class methods
+        /*
+        Constructor for GUI class.
+        */
+        GUI::GUI()
+        {
+                init();
+        }
+
+        /*
+        Destructor for GUI class.
+        */
+        GUI::~GUI()
+        {
+        }
+// GUI methods
+        /*
+        Render the GUI.
+        */
+        void GUI::render()
+        {
+                // TODO: Implement render method
+        }
+
+        /*
+        Update the window.
+        */
+        void GUI::update()
+        {
+                // TODO: Implement update method
+        }
+
+        /*
+        Wait for an event to occur.
+        */
+        void GUI::waitEvent()
+        {
+                // TODO: Implement waitEvent method
+        }
+} // namespace System
