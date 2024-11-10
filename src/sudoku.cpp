49e31c7dc3ffd6199dbc8bdd1d712f966ed89c03
Roiqk7 <petrsevecek@icloud.com>
2024-11-10 09:02:10 +0100

    Minimized - removed test code

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -6,11 +6,6 @@ Description: This file contains the main function.
 
 #include "../include/globals.hpp"
 
-// Delete - Test
-#include "../include/grid.hpp"
-#include "../include/generator.hpp"
-#include <array>
-
 int main()
 {
         LOG_TRACE("main() called");
@@ -18,16 +13,6 @@ int main()
         #ifdef DEVELOPMENT
         SET_LOG_LEVEL_DEBUG();
         #endif // DEVELOPMENT
-
-        // Delete - Test
-        Sudoku::Grid grid;
-        Sudoku::Generator generator;
-
-        for (int i = 0; i < 9; i++)
-        {
-                generator.generate(grid);
-                grid.print();
-        }
 
         return 0;
 }
