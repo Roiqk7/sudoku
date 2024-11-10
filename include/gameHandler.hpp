f6b1c05bab0540d2cb58135d92cd8235c3776f68
Roiqk7 <petrsevecek@icloud.com>
2024-11-10 13:59:28 +0100

    Added

---
+++ include/gameHandler.hpp
@@ -0,0 +1,56 @@
+/*
+Date: 10/11/2024
+
+Game handler makes the connection between the GUI and the game logic.
+*/
+
+#ifndef GAME_HANDLER_HPP
+#define GAME_HANDLER_HPP
+
+#include "generator.hpp"
+#include "grid.hpp"
+#include "solver.hpp"
+
+namespace Sudoku
+{
+        /*
+        Number of given based on difficulty:
+        Easy: 34-38
+        Medium: 29-33
+        Hard: 24-28
+        Expert: 19-23
+
+        @note NONE is used for the default constructor.
+        */
+        enum class Difficulty
+        {
+                NONE = 0,
+                EASY = 36,
+                MEDIUM = 31,
+                HARD = 26,
+                EXPERT = 21
+        };
+
+        class GameHandler
+        {
+        public: // Methods
+        // Class methods
+                GameHandler();
+                ~GameHandler();
+        // Game handler methods
+                void newGame(Difficulty difficulty);
+                bool checkUserInput(int row, int col, int value);
+                void solve(int cells);
+        // Getters
+                void getGrid(Grid& grid, bool solved = false) const;
+        // Setters
+                void setGrid(const Grid& grid);
+        private: // Variables
+                Grid grid;                                                      // What the player sees
+                Grid solved;                                                    // The solved grid
+                Generator generator;                                            // Generates a new sudoku grid
+                Solver solver;                                                  // Solves the sudoku grid
+        };
+}
+
+#endif // !GAME_HANDLER_HPP
---
+++ src/gameHandler.cpp
@@ -0,0 +1,160 @@
+/*
+Date: 10/11/2024
+
+Game handler makes the connection between the GUI and the game logic.
+*/
+
+#include "../include/gameHandler.hpp"
+#include "../include/generator.hpp"
+#include "../include/globals.hpp"
+#include "../include/grid.hpp"
+#include "../include/solver.hpp"
+#include <algorithm>
+#include <random>
+
+namespace Sudoku
+{
+// Class methods
+        /*
+        Constructor. Initializes the grid, generator, and solver.
+
+        @note Does not initialize the grid with a new game.
+        */
+        GameHandler::GameHandler()
+        {
+                LOG_TRACE("GameHandler::GameHandler() called");
+
+                // Initialize the variables
+                grid = Grid();
+                generator = Generator();
+                solver = Solver();
+        }
+
+        /*
+        Destructor.
+        */
+        GameHandler::~GameHandler()
+        {
+                LOG_TRACE("GameHandler::~GameHandler() called");
+        }
+// Game handler methods
+        /*
+        Generates a new game with the given difficulty.
+
+        @param difficulty The difficulty of the game.
+        */
+        void GameHandler::newGame(Difficulty difficulty)
+        {
+                LOG_TRACE("GameHandler::newGame() called");
+
+                // Generate a new grid
+                generator.generate(grid);
+
+                // Set the solved grid
+                solved = grid;
+
+                // Remove cells based on the difficulty
+                int cells = static_cast<int>(difficulty);
+                grid.setZero(cells);
+        }
+
+        /*
+        Checks if the user input is correct and sets the value in the grid if it is.
+
+        @param row The row of the cell.
+        @param col The column of the cell.
+        @param value The value to check.
+
+        @return True if the value is correct, false otherwise.
+        */
+        bool GameHandler::checkUserInput(int row, int col, int value)
+        {
+                LOG_TRACE("GameHandler::checkUserInput() called");
+
+                // Check if the value is correct
+                bool isCorrect = grid.isValidValue(row, col, value);
+
+                // If the value is correct, set it in the grid
+                if (isCorrect)
+                {
+                        grid.setCell(row, col, value);
+                }
+
+                return isCorrect;
+        }
+
+        /*
+        Solves the given number of cells in the grid.
+
+        @param cells The number of cells to solve.
+
+        @note If cells == 81, the entire grid is solved.
+        */
+        void GameHandler::solve(int cells)
+        {
+                LOG_TRACE("GameHandler::solve() called");
+
+                // Find unsolved cells
+                std::vector<int> unsolvedCells;
+                for (int i = 0; i < 81; i++)
+                {
+                        if (grid.getCell(i) == 0)
+                        {
+                                unsolvedCells.push_back(i);
+                        }
+                }
+
+                // Solve the grid if the number of cells is invalid
+                // or the entire grid is to be solved
+                if (cells == 81 || cells >= static_cast<int>(
+                        unsolvedCells.size()) || cells <= 0)
+                {
+                        grid = solved;
+                        return;
+                }
+
+                // Shuffle the unsolved cells
+                std::shuffle(unsolvedCells.begin(), unsolvedCells.end(),
+                        std::mt19937(std::random_device()()));
+
+                // Solve the desired number of cells
+                int cell;
+                for (int i = 0; i < cells; i++)
+                {
+                        cell = unsolvedCells[i];
+                        grid.setCell(cell, solved.getCell(cell));
+                }
+        }
+// Getters
+        /*
+        Gets the grid.
+
+        @param grid The grid to store the values in.
+        @param solved True if the solved grid is to be returned, false otherwise.
+        */
+        void GameHandler::getGrid(Grid& grid, bool solved) const
+        {
+                LOG_TRACE("GameHandler::getGrid() called");
+
+                if (solved)
+                {
+                        grid = this->solved;
+                }
+                else
+                {
+                        grid = this->grid;
+                }
+        }
+// Setters
+        /*
+        Sets the grid.
+
+        @param grid The grid to set.
+        */
+        void GameHandler::setGrid(const Grid& grid)
+        {
+                LOG_TRACE("GameHandler::setGrid() called");
+
+                this->grid = grid;
+        }
+}
