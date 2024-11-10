1cedbd812bd936018a43273f0d67127787b48b2f
Roiqk7 <petrsevecek@icloud.com>
2024-11-10 09:10:47 +0100

    Added

---
+++ test/src/solver.cpp
@@ -0,0 +1,35 @@
+/*
+Date: 10/11/2024
+
+This file provides a few tests for the Solver class.
+*/
+
+#ifndef TEST
+#error "Tried to compile a test file without the TEST macro defined. Aborting compilation."
+#else
+
+#include "../../include/grid.hpp"
+#include "../../include/solver.hpp"
+#include <array>
+#include <gtest/gtest.h>
+
+/*
+Test for the Solver::solve() method.
+
+Expected: No exceptions are thrown and the grid is solved.
+*/
+TEST(Solver, Solve)
+{
+        Sudoku::Grid grid;
+        Sudoku::Solver solver;
+
+        std::array<int, 9> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        grid.setBox(0, arr);
+        grid.setBox(4, arr);
+        grid.setBox(8, arr);
+
+        ASSERT_TRUE(solver.solve(grid));
+        ASSERT_TRUE(grid.isSolved());
+}
+
+#endif // !TEST
