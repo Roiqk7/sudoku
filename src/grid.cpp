2726f2dd39ce71c6687425db24a56a522240e062
Roiqk7 <petrsevecek@icloud.com>
2024-11-10 08:46:29 +0100

    Removed newline after print()

--- src/grid.cpp
+++ src/grid.cpp
@@ -325,7 +325,6 @@ namespace Sudoku
                                 std::cout << "------|-------|------" << std::endl;
                         }
                 }
-                std::cout << std::endl;
         }
 
         /*
