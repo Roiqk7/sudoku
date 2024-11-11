35741984113e50302aabffed2721bc61dc6a9ef1
Roiqk7 <petrsevecek@icloud.com>
2024-11-11 21:46:21 +0100

    Implemented derived classes from object

--- include/object.hpp
+++ include/object.hpp
@@ -18,7 +18,7 @@ namespace System
         enum class ObjectType
         {
                 NONE = 0,
-                TEXTURE,
+                SPRITE,
                 FONT,
                 RECTANGLE
         };
@@ -44,6 +44,57 @@ namespace System
                 int width;                                              // Width of the object
                 int height;                                             // Height of the object
                 std::filesystem::path path;                             // Path to the resource
+        };
+
+        class Sprite : public Object
+        {
+        public: // Methods
+        // Class methods
+                Sprite(int x, int y, int width, int height,
+                        std::filesystem::path path, sf::Texture texture,
+                        int xScale, int yScale);
+                ~Sprite() override;
+        // Object methods
+                void load() override;
+                void render(sf::RenderWindow& window) override;
+        private: // Variables
+                sf::Texture texture;                                    // Texture of the sprite
+                sf::Sprite sprite;                                      // Sprite to render
+                int xScale;                                             // X scale of the sprite
+                int yScale;                                             // Y scale of the sprite
+        };
+
+        class Font : public Object
+        {
+        public: // Methods
+        // Class methods
+                Font(int x, int y, int width, int height,
+                        std::filesystem::path path, sf::Font font,
+                        sf::Text text, int fontSize);
+                ~Font() override;
+        // Object methods
+                void load() override;
+                void render(sf::RenderWindow& window) override;
+        private: // Variables
+                sf::Font font;                                          // Font of the text
+                sf::Text text;                                          // Text to render
+                int fontSize;                                           // Size of the font
+        };
+
+        class Rectangle : public Object
+        {
+        public: // Methods
+        // Class methods
+                Rectangle(int x, int y, int width, int height,
+                        std::filesystem::path path, sf::RectangleShape rectangle,
+                        sf::Color color);
+                ~Rectangle() override;
+        // Object methods
+                void load() override;
+                void render(sf::RenderWindow& window) override;
+        private: // Variables
+                sf::RectangleShape rectangle;                           // Rectangle to render
+                sf::Color color;                                        // Color of the rectangle
         };
 }
 
