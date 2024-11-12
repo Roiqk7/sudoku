6db529c7a8b1b95c2d2b8f5773d0f9015ff741cd
Roiqk7 <petrsevecek@icloud.com>
2024-11-12 11:45:13 +0100

    Layed down the basics

--- include/scene.hpp
+++ include/scene.hpp
@@ -7,6 +7,25 @@ Scene class stores one scene of the application and all the objects in it.
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
+#include "object.hpp"
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace System
+{
+        class Scene
+        {
+        public: // Methods
+
+        public: // Variables
+                std::string name;                                       // Name of the scene (unique)
+        private: // Variables
+                std::vector<std::shared_ptr<Object>> objects;           // Objects in the scene
+                // ? Add an interval tree for click detection and
+                // ? simple action mapping to the clicked object
+        };
+}
 
 
 #endif // !SCENE_HPP
