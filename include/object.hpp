e546ac11686bb39234cfd4a9b668940db2703dcb
Roiqk7 <petrsevecek@icloud.com>
2024-11-12 07:53:58 +0100

    Added name to the objects as id and implemented all objects

--- include/object.hpp
+++ include/object.hpp
@@ -12,6 +12,7 @@ Object class stores the properties of an object in the scene.
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
+#include <string>
 
 namespace System
 {
@@ -19,15 +20,15 @@ namespace System
         {
                 NONE = 0,
                 SPRITE,
-                FONT,
+                TEXT,
                 RECTANGLE
         };
-
+// Object class
         class Object
         {
         public: // Methods
         // Class methods
-                Object(int x, int y, int width, int height,
+                Object(const std::string& name, int x, int y,
                         std::filesystem::path path, ObjectType type);
                 virtual ~Object() = default;
         // Object methods
@@ -35,24 +36,26 @@ namespace System
                 virtual void render(sf::RenderWindow& window) = 0;
         public: // Variables
         // Resource specific
-                ObjectType type;                                        // Type of the object
+                ObjectType type;                                       // Type of the object
         protected: // Variables
         // General
-                bool loaded;                                            // If the object is loaded
+                std::string name;                                      // Name of the object
+                std::filesystem::path path;                            // Path to the resource
+                bool loaded = false;                                   // If the object is loaded
+        // Rendering
                 int x;                                                  // X position of the object
                 int y;                                                  // Y position of the object
                 int width;                                              // Width of the object
                 int height;                                             // Height of the object
-                std::filesystem::path path;                             // Path to the resource
         };
-
+// Sprite class
         class Sprite : public Object
         {
         public: // Methods
         // Class methods
-                Sprite(int x, int y, int width, int height,
+                Sprite(const std::string& name, int x, int y,
                         std::filesystem::path path, sf::Texture texture,
-                        int xScale, int yScale);
+                        float xScale, float yScale);
                 ~Sprite() override;
         // Object methods
                 void load() override;
@@ -60,39 +63,41 @@ namespace System
         private: // Variables
                 sf::Texture texture;                                    // Texture of the sprite
                 sf::Sprite sprite;                                      // Sprite to render
-                int xScale;                                             // X scale of the sprite
-                int yScale;                                             // Y scale of the sprite
+                float xScale;                                           // X scale of the sprite
+                float yScale;                                           // Y scale of the sprite
         };
-
-        class Font : public Object
+// Font class
+        class Text : public Object
         {
         public: // Methods
         // Class methods
-                Font(int x, int y, int width, int height,
+                Text(const std::string& name, int x, int y,
                         std::filesystem::path path, sf::Font font,
                         sf::Text text, int fontSize);
-                ~Font() override;
+                ~Text() override;
         // Object methods
                 void load() override;
                 void render(sf::RenderWindow& window) override;
         private: // Variables
-                sf::Font font;                                          // Font of the text
                 sf::Text text;                                          // Text to render
+                sf::Font font;                                          // Font of the text
                 int fontSize;                                           // Size of the font
         };
-
+// Rectangle class
         class Rectangle : public Object
         {
         public: // Methods
         // Class methods
-                Rectangle(int x, int y, int width, int height,
-                        std::filesystem::path path, sf::RectangleShape rectangle,
-                        sf::Color color);
+                Rectangle(const std::string& name, int x, int y,
+                        std::filesystem::path path, int width, int height,
+                        sf::RectangleShape rectangle, sf::Color color);
                 ~Rectangle() override;
         // Object methods
                 void load() override;
                 void render(sf::RenderWindow& window) override;
         private: // Variables
+                int width;                                              // Width of the object
+                int height;                                             // Height of the object
                 sf::RectangleShape rectangle;                           // Rectangle to render
                 sf::Color color;                                        // Color of the rectangle
         };
