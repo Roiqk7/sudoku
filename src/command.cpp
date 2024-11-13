30809f268bae207762aefc090340288968ada8d6
Roiqk7 <petrsevecek@icloud.com>
2024-11-13 19:53:10 +0100

    Added

---
+++ src/command.cpp
@@ -0,0 +1,31 @@
+/*
+Date: 13/11/2024
+
+Command class used to store commands for buttons in the scene. Following the command pattern.
+*/
+
+#include "../include/command.hpp"
+#include "../include/macros.hpp"
+#include <functional>
+
+namespace System
+{
+        /*
+        Constructor for the Command class.
+        */
+        Command::Command(std::function<void()> command)
+                : command(command)
+        {
+                LOG_TRACE("Command::Command() called");
+        }
+
+        /*
+        Execute the command.
+        */
+        void Command::execute()
+        {
+                LOG_TRACE("Command::execute() called");
+
+                command();
+        }
+}
