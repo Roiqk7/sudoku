f592eb6f2647c584ef9af19661d14cc4502b3225
Roiqk7 <petrsevecek@icloud.com>
2024-11-14 14:44:53 +0100

    Finally implemented (somewhat ugly approach for the clickable objects)

--- include/scene.hpp
+++ include/scene.hpp
@@ -7,6 +7,7 @@ Scene class stores one scene of the application and all the objects in it.
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
+#include "command.hpp"
 #include "object.hpp"
 #include <memory>
 #include <string>
@@ -17,11 +18,23 @@ namespace System
         class Scene
         {
         public: // Methods
-
+        // Class methods
+                Scene(const std::string& name);
+                ~Scene();
+        // Scene methods
+                void addObject(const std::shared_ptr<Object>& object);
+                void addClickableObject(const std::unique_ptr<Rectangle>& object);
+                void retrieveClickedCommand(int x, int y,
+                        std::weak_ptr<Command>& command);
+        private: // Methods
+                bool checkClicked(const std::unique_ptr<Rectangle>&
+                        object, int x, int y);
         public: // Variables
                 std::string name;                                       // Name of the scene (unique)
         private: // Variables
                 std::vector<std::shared_ptr<Object>> objects;           // Objects in the scene
+                std::vector<std::unique_ptr<Rectangle>>
+                        clickableObjects;                               // Clickable objects in the scene
         };
 }
 
