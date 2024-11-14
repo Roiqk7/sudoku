2039b5ee38d6ada15b97dcef385edcffdfc8ff21
Roiqk7 <petrsevecek@icloud.com>
2024-11-14 14:10:39 +0100

    Removed interval tree - useless

--- include/scene.hpp
+++ include/scene.hpp
@@ -7,7 +7,6 @@ Scene class stores one scene of the application and all the objects in it.
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
-#include "intervalTree.hpp"
 #include "object.hpp"
 #include <memory>
 #include <string>
@@ -23,7 +22,6 @@ namespace System
                 std::string name;                                       // Name of the scene (unique)
         private: // Variables
                 std::vector<std::shared_ptr<Object>> objects;           // Objects in the scene
-                Tool::IntervalTree intervalTree;                        // Stored all the objects in the scene
         };
 }
 
