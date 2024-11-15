2ffd39705cc52ac7f4d9517abe73c614ecfc9603
Roiqk7 <petrsevecek@icloud.com>
2024-11-15 16:40:16 +0100

    Updated

--- src/scenes.cpp
+++ src/scenes.cpp
@@ -12,12 +12,14 @@ This file contains pre-made scenes for the GUI.
 #include "../include/scene.hpp"
 #include "../include/scenes.hpp"
 #include <chrono>
+#include <filesystem>
 #include <functional>
 #include <memory>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace System
@@ -39,14 +41,21 @@ namespace System
 
                 // Text
                 auto center = getWindowCenter(getWindowSize(window));
+                auto font = getFont("font");
                 std::shared_ptr<Object> text = std::make_shared<Text>(
                         "Default scene text", center.x, center.y,
+                        font.first, font.second,
                         "THIS SHOULD BE HIDDEN", Colors::WHITE, 50);
                 scene.addObject(text);
 
                 // Background
+                auto sizeU = getWindowSize(window);
+                sf::Vector2i size(sizeU.x, sizeU.y);
+                sf::Vector2i topLeft = getWindowTopLeftCorner();
+                sf::RectangleShape backgroundRect(sf::Vector2f(size.x, size.y));
                 std::shared_ptr<Object> background = std::make_shared<Rectangle>(
-                        "Background", 0, 0, 1920, 1080, Colors::BLACK);
+                        "Background", topLeft.x, topLeft.y, size.x, size.y,
+                        backgroundRect, Colors::BLACK);
                 scene.addObject(background);
 
                 // Set the update function
