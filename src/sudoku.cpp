c2ffaf08c0507cc2a88dc096ec37bae9deb98b6a
Ševěček Petr <sevecekpeta@gmail.com>
2024-12-01 15:22:48 +0100

    Added a file logging option

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -8,12 +8,18 @@ Description: This file contains the main function.
 #include "../include/macros.hpp"
 #include "../include/signalHandler.hpp"
 #include <csignal>
+#include <filesystem>
 
 int main()
 {
         #ifdef DEVELOPMENT
+        // Note: Uncomment the following lines to enable logging to a file
+        // std::filesystem::create_directories("log");
+        // std::filesystem::path logPath = std::filesystem::current_path() / "log" / "log.txt";
+        // SET_LOG_FILE(logPath.string());
         SET_LOG_LEVEL_DEBUG();
         LOG_TRACE("main() called");
+        // LOG_DEBUG("Log file set to: {}", logPath.string());
         #endif // DEVELOPMENT
 
         // Set up signal handling
